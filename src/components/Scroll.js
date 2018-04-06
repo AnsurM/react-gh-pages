1f06ac7c3e71f80bf34026051f776e3d5f892b20
AnsurM <ansurmehdi@gmail.com>
2018-04-06 20:45:35 +0500

    Create a react app and publish it to GitHub pages

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return (
-			<div style = {{overflowY: 'scroll', border: '2px solid orange', height: '550px'}}>
+			<div style = {{overflowY: 'scroll', border: '2px solid orange', height: '650px', width: '1400px', margin: '50px 250px 50px 250px'}}>
 				{props.children}
 			</div>
 		)
